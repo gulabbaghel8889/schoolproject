b7636b17f58dbc578335d060168ad07cbe9823dc
gulabbaghel8889 <gulabbaghel8889@gmail.com>
2026-03-06 20:42:57 +0530

    fix static files

--- schoolproject/settings.py
+++ schoolproject/settings.py
@@ -43,10 +43,21 @@ INSTALLED_APPS = [
 # MIDDLEWARE
 # =========================
 
+# MIDDLEWARE = [
+#     'django.middleware.security.SecurityMiddleware',
+
+#     # static files serve karega (Render ke liye important)
+#     'whitenoise.middleware.WhiteNoiseMiddleware',
+
+#     'django.contrib.sessions.middleware.SessionMiddleware',
+#     'django.middleware.common.CommonMiddleware',
+#     'django.middleware.csrf.CsrfViewMiddleware',
+#     'django.contrib.auth.middleware.AuthenticationMiddleware',
+#     'django.contrib.messages.middleware.MessageMiddleware',
+#     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+# ]
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-
-    # static files serve karega (Render ke liye important)
     'whitenoise.middleware.WhiteNoiseMiddleware',
 
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -56,7 +67,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 
 ROOT_URLCONF = 'schoolproject.urls'
 
@@ -133,11 +143,19 @@ USE_TZ = True
 # STATIC FILES
 # =========================
 
+# STATIC_URL = '/static/'
+
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static",
+# ]
+
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
